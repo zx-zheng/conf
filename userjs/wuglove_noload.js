21a9dc5065f1d6495a16a1d60678e57a08d2cc71
Zhongxiang Zheng <zheng.z.aa@gmail.com>
2018-01-14 19:21:40 +0900

    Create wuglove_noload.js

---
+++ userjs/wuglove_noload.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        wuglove no load
+// @author      zzheng
+// @namespace   
+// @description 
+// @include     https://finn-neo.com/*
+// @exclude     
+// ==/UserScript==
+
+(function () {
+    clearInterval(Timer);
+    $('#progress').hide();
+})();
